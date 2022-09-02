3e015b1e91e58b124d4d934394ec9ef966657934
VladimirR <Vova.ru0007@yandex.ru>
2022-09-02 15:14:16 +0500

    final commit2

---
+++ C#9SEMINAR/Zad2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+Console.Clear();
+Console.WriteLine ($"Введите  число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ($"Введите  число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+string Number(int numberA , int numberB)
+{
+    if (numberA < numberB)
+    {
+        return $"{numberA } " + Number(numberA  + 1, numberB);
+    }
+    else
+    {
+        return $"{numberB}";
+    }
+}
+Console.WriteLine(Number(numberA, numberB));
