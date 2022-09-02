d5dd62ea06de4648e01540ac08848195d75114a6
VladimirR <Vova.ru0007@yandex.ru>
2022-09-02 15:14:23 +0500

    final commit3

---
+++ C#9SEMINAR/Zad3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+Console.Clear();
+Console.WriteLine($"Введите  число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return number % 10 + Sum(number / 10);
+    }
+}
+Console.WriteLine(Sum(number));
