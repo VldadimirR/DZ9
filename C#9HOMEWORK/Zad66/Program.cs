017f83edf3db770c6ee9c98588472b346901a88b
VladimirR <Vova.ru0007@yandex.ru>
2022-09-02 16:15:36 +0500

    zad66

---
+++ C#9HOMEWORK/Zad66/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+: Задайте значения M и N. Напишите программу, которая найдёт сумму чётных чисел натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 56
+M = 4; N = 8. -> 18
+*/
+Console.Clear();
+Console.WriteLine($"Введите  число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите  число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int numberA, int numberB)
+{
+    if (numberA % 2 != 0)
+    {
+        numberA++;
+    }
+    if (numberA <= numberB)
+    {
+        return numberA + Sum(numberA + 2, numberB);
+    }
+    else
+    {
+        return 0;
+    }
+}
+Console.WriteLine(Sum(numberA, numberB));
